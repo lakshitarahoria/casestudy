4c32c955201ba650e3eeaaacda6016baaff084f2
lakshitarahoria <36448998+lakshitarahoria@users.noreply.github.com>
2021-09-15 10:00:44 +0530

    Update Client.java

--- BankApplication2/src/com/accountmanagement/ui/Client.java
+++ BankApplication2/src/com/accountmanagement/ui/Client.java
@@ -1,32 +1,112 @@
 package com.accountmanagement.ui;
-
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.Map.Entry;
-
-import com.accountmanagement.model.AccountDetails;
 import com.accountmanagement.service.ServiceImpl;
+import com.accountmanagement.dao.Dao;
 
 //end user interacts with client and we take info from client side and pass it to service then to dao 
 public class Client {
-
+	public static void main(String[] args) {
 	//switch case for different operations
 	//output required- 
-	
-//	"Welcome to Bank Application"
-//	
+//		"Welcome to Bank Application"
+	System.out.println("Welcome to Bank Application");
+	ServiceImpl ca=new ServiceImpl();
+	Dao data=new Dao();
+	Scanner sc=new Scanner(System.in);
+    String cust_name;
+    long mob_number;
+    String acc_type;
+    int accountnum;
+    String branch;
+    float balance;
+    boolean exit=false;
+    while(exit!=true)
+    {
+    System.out.println("1.Create Account\n2.Deposit\n3.Withdrawal\n4.Trasfer\n5.Transaction History\n6.Show Details\n7.Exit\n");
+    System.out.println("Enter the choice: ");
+    int choice=sc.nextInt();
+    switch(choice)
+    {
 //	1.Account creation - 
 //  further fields will be required. first is name - We need to use Regex , it has to be alphabetics only , First letter capital 
-	
-//	and contact number- 10 digit number which start from numbers between 6-9
-//	enter account holders address- string
-//	enter branch name - branch address.String
-//	type of account -only 2 kinds allowed, current and savings
-//	balance - default 5000 but you can add anything more
-//	
-//	
+        case 1:
+                System.out.println("Enter the Customer name: ");
+                cust_name=sc.next();
+                ca.customername(cust_name);
+                
+                System.out.println("Enter the Mobile Number: ");
+                mob_number=sc.nextLong();
+                ca.customermobile(mob_number);
+                
+                System.out.println("Enter the Account Type: ");
+                acc_type=sc.next();
+                ca.accountType(acc_type);
+                
+                System.out.println("Enter the Branch: ");
+                branch=sc.next();
+                ca.branchName(branch);
+                
+                System.out.println("Enter the Balance: ");
+                balance=sc.nextFloat();
+                //String accountType,String bankBranch,float balance,int contactNumber,String name
+                data.add(acc_type,branch,balance,mob_number,cust_name);
+                break;
+        case 2:
+            System.out.println("Enter Account Number: ");
+            
+            
+            System.out.println("Enter Deposit Amount: ");
+
+            
+            System.out.println("Amount Deposit successful");
+            System.out.println("Remaining Balance is");
+            break;
+        case 3:
+            System.out.println("Enter Account Number: ");
+            
+            
+            System.out.println("Enter Withdrawal Amount: ");
+
+            
+            System.out.println("Amount Withdrawal successful");
+            System.out.println("Remaining Balance is");
+            break;
+        case 4:
+            System.out.println("Enter Account Number: ");
+            
+            System.out.println("Enter 3rd Party Account Number: ");
+            
+            System.out.println("Enter Transfer Amount: ");
+
+            
+            System.out.println("Amount Transfer successful");
+            System.out.println("Remaining Balance is");
+            break;
+       case 5:
+            System.out.println("Enter Account Number: ");
+            
+            
+            System.out.println("Transaction History:\n");
+
+            
+            System.out.println("Amount Deposit successful");
+            System.out.println("Remaining Balance is");
+            break;
+       case 6:
+           System.out.println("Enter Account Number: ");
+           //accountnum=sc.nextInt();
+           data.ShowDet();
+           
+           System.out.println("Details: \n");
+
+           break;
+       case 7:
+    	   exit=true;
+    	   break;
+    }
+}
+
+
 	
 	
 //	2. Deposit amount
@@ -54,5 +134,5 @@ public class Client {
 //	6.Show transaction history (all fund transfers)
 //	7.Exit
 //	
-
+	}
 }
